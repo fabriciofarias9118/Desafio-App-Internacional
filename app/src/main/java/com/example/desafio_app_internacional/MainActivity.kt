16a7b6834a35fa67d7907e8e23998b358aee045f
fabricio <fabriciofarias9118@gmail.com>
2023-09-11 22:55:49 -0300

    Initial commit

---
+++ app/src/main/java/com/example/desafio_app_internacional/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.desafio_app_internacional
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        throw Exception("bug")
+    }
+}
